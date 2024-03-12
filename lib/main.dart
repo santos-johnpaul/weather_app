388c289523431231d77b1811c16a060cfe60ae9b
Nicoline <nicolinemayanncortez@gmail.com>
2024-03-12 17:58:46 +0800

    nicoline added search bar

--- lib/main.dart
+++ lib/main.dart
@@ -30,3 +30,39 @@ class _WeatherAppState extends State<WeatherApp> {
   String apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   TextEditingController locationController = TextEditingController();
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+        leading: Icon(Icons.cloud),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: locationController,
+              decoration: InputDecoration(
+                labelText: 'Enter Location',
+                prefixIcon: Icon(Icons.location_city),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                fetchWeatherData(locationController.text).then((weatherData) {
+                  setState(() {});
+                }).catchError((error) {
+                  print('Error: $error');
+                });
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.search),
+                  SizedBox(width: 8),
+                  Text('Search'),
+                ],
+              ),
+            ),
